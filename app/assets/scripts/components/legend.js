fbfc68696905f7118f68ca4053669e07dcc5e52c
Nick Bumbarger <nbumbarger@gmail.com>
2016-08-25 11:32:39 -0400

    Polished legend

--- app/assets/scripts/components/legend.js
+++ app/assets/scripts/components/legend.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import chroma from 'chroma-js'
+
+const scale = chroma.scale(['#F2F2F2', '#686868'])
 
 export default function () {
   return (
     <div className='legend'>
-      <div className='legend-title'>Supply and Demand Index</div>
+      <div className='legend-title'>
+        <h3>Supply and Demand Index</h3>
+        <h4>This index combines xxx, xxx, xxx, xxx and xxx.</h4>
+      </div>
       <div className='legend-scale'>
       <ul className='legend-labels'>
-        <li><span style={{background: '#F1EEF6'}}></span>less need</li>
-        <li><span style={{background: '#BDC9E1'}}></span></li>
-        <li><span style={{background: '#74A9CF'}}></span></li>
-        <li><span style={{background: '#2B8CBE'}}></span></li>
-        <li><span style={{background: '#045A8D'}}></span>more need</li>
+        <li><span style={{background: scale(0.2).hex()}}></span>
+          <span style={{marginTop: '6px'}}>less need</span>
+        </li>
+        <li><span style={{background: scale(0.4).hex()}}></span></li>
+        <li><span style={{background: scale(0.6).hex()}}></span></li>
+        <li><span style={{background: scale(0.8).hex()}}></span></li>
+        <li><span style={{background: scale(1).hex()}}></span>
+          <span style={{marginTop: '6px'}}>more need</span>
+        </li>
       </ul>
       </div>
     </div>
