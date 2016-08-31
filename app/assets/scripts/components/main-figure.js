19e0297969baccdf65b9f4b306c71e1c503d3c81
Nick Bumbarger <nbumbarger@gmail.com>
2016-08-31 16:45:35 -0400

    Consolidated main figure (removes main-figure-top)

--- app/assets/scripts/components/main-figure.js
+++ app/assets/scripts/components/main-figure.js
@@ -5,14 +5,48 @@ import { connect } from 'react-redux'
 
 import { } from '../actions'
 
-import Top from './main-figure-top'
+import BarChart from './main-figure-bar-chart'
 
 const MainFigure = React.createClass({
   render: function () {
     return (
       <div className='main-figure'>
         <div className='main-figure-interior'>
-          <Top />
+        <div className='main-figure-top'>
+          <div className='back-link'>
+            <a href='#'>Back to All Areas</a>
+          </div>
+            <h1 className='zipcode-title'>Supply and demand for 700819</h1>
+            <dl className='dl-horizontal'>
+              <dt className='med-income data-number'>$23,901</dt>
+              <dd className='data-description'>Median Income</dd>
+            </dl>
+            <dl className='summary-stat'>
+              <dt className='fam-below-poverty data-number'>452,192</dt>
+              <dd className='data-description'>Families Below Poverty</dd>
+            </dl>
+            <dl className='summary-stat'>
+              <dt className='school-aged-children data-number'>12,192</dt>
+              <dd className='data-description'>Total School Aged Children</dd>
+            </dl>
+            <dl className='summary-stat'>
+              <dt className='dropout-factories data-number'>5</dt>
+              <dd className='data-description'>Dropout Factories</dd>
+            </dl>
+            <dl className='summary-stat'>
+              <dt className='feeder-schools data-number'>5</dt>
+              <dd className='data-description'>Feeder Schools</dd>
+            </dl>
+            <div className='barchart-total'>
+              <BarChart
+                students={45}
+                studentPercent={90}
+                slots={70}
+                slotPercent={45}
+                zipCode={'20001'}
+              />
+            </div>
+        </div>
         </div>
       </div>
     )
