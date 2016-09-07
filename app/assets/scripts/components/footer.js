3e8b3a0769270a813627e6e581f0b6d22037ad92
Anna Scalamogna <anna@developmentseed.org>
2016-09-07 16:13:27 -0400

    update footer styling

--- app/assets/scripts/components/footer.js
+++ app/assets/scripts/components/footer.js
@@ -14,10 +14,7 @@ export default function () {
           <p>4. Data for “students,” “slots,” “programs” and “funds” generated from funder responses to internal questionnaire.</p>
         <h1 className='heading-alt'>Notes</h1>
         <p>The index depicts the extent of the gap between the number of available slots and the number of students served in out-of-school time programs in Harris County. In addition to this base layer of slots versus students, we have also included additional, socioeconomic and educational data to further contextualize a particular neighborhood’s need for out-of-school time programs.</p>
-      </div>
-      <div className='footer-links'>
-        <h1 className='heading-alt'>More Information</h1>
-        <a href='http://houstonendowment.org'>Contact Us</a>
+         <a href='http://houstonendowment.org'>Contact Us</a>
       </div>
     </div>
   )
