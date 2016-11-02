d10e438d99fa1e35ddcd1af75fc3a71cc393f6f0
Jefferson Stovall <stovall@collaborativecommunications.com>
2016-11-02 07:21:54 -0400

    Update header.js

--- app/assets/scripts/components/header.js
+++ app/assets/scripts/components/header.js
@@ -5,7 +5,7 @@ export default function () {
     <div className='header-container'>
       <div className='header'>
         <div className='header-copy'>
-           /**<h2 className='heading-alt'>Houston Endowment</h2>**/
+           {/*<h2 className='heading-alt'>Houston Endowment</h2>*/}
           <h1>Mapping Need for Out-of-School Time Programs in Harris County</h1>
           <p>This tool assesses the degree of need for out-of-school time programs in nine select neighborhoods across Harris County. In displaying the varying resources and socioeconomic factors across these zip codes, we hope that this map guides the Houston out-of-school time community to effectively allocate funds to support its highest-need youth.</p>
           <div className='share-container'>
